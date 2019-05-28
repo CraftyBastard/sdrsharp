6d6a694872759664981bdeb7cf8225eed728d30c
Горлов Максим <gorlov.maxim@gmail.com>
2019-05-28 15:24:04 +0300

    Fix hidden buttons in Frequency manager

--- FrequencyManager/DialogEntryInfo.Designer.cs
+++ FrequencyManager/DialogEntryInfo.Designer.cs
@@ -48,9 +48,9 @@
             ((System.ComponentModel.ISupportInitialize)(this.shiftNumericUpDown)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nudFilterBandwidth)).BeginInit();
             this.SuspendLayout();
-            // 
+            //
             // label1
-            // 
+            //
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(10, 11);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
@@ -58,9 +58,9 @@
             this.label1.Size = new System.Drawing.Size(250, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Select an existing group or enter a new group name";
-            // 
+            //
             // label2
-            // 
+            //
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(10, 37);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
@@ -68,9 +68,9 @@
             this.label2.Size = new System.Drawing.Size(39, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "Group:";
-            // 
+            //
             // label3
-            // 
+            //
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(10, 67);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
@@ -78,9 +78,9 @@
             this.label3.Size = new System.Drawing.Size(38, 13);
             this.label3.TabIndex = 2;
             this.label3.Text = "Name:";
-            // 
+            //
             // label4
-            // 
+            //
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(10, 98);
             this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
@@ -88,9 +88,9 @@
             this.label4.Size = new System.Drawing.Size(60, 13);
             this.label4.TabIndex = 3;
             this.label4.Text = "Frequency:";
-            // 
+            //
             // label5
-            // 
+            //
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(11, 187);
             this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
@@ -98,17 +98,17 @@
             this.label5.Size = new System.Drawing.Size(37, 13);
             this.label5.TabIndex = 4;
             this.label5.Text = "Mode:";
-            // 
+            //
             // lblMode
-            // 
+            //
             this.lblMode.Location = new System.Drawing.Point(89, 187);
             this.lblMode.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblMode.Name = "lblMode";
             this.lblMode.Size = new System.Drawing.Size(120, 17);
             this.lblMode.TabIndex = 5;
-            // 
+            //
             // comboGroupName
-            // 
+            //
             this.comboGroupName.FormattingEnabled = true;
             this.comboGroupName.Location = new System.Drawing.Point(85, 34);
             this.comboGroupName.Margin = new System.Windows.Forms.Padding(2);
@@ -116,18 +116,18 @@
             this.comboGroupName.Size = new System.Drawing.Size(178, 21);
             this.comboGroupName.TabIndex = 0;
             this.comboGroupName.TextChanged += new System.EventHandler(this.Control_TextChanged);
-            // 
+            //
             // textBoxName
-            // 
+            //
             this.textBoxName.Location = new System.Drawing.Point(85, 64);
             this.textBoxName.Margin = new System.Windows.Forms.Padding(2);
             this.textBoxName.Name = "textBoxName";
             this.textBoxName.Size = new System.Drawing.Size(178, 20);
             this.textBoxName.TabIndex = 1;
             this.textBoxName.TextChanged += new System.EventHandler(this.Control_TextChanged);
-            // 
+            //
             // frequencyNumericUpDown
-            // 
+            //
             this.frequencyNumericUpDown.Increment = new decimal(new int[] {
             10,
             0,
@@ -151,9 +151,9 @@
             this.frequencyNumericUpDown.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             this.frequencyNumericUpDown.ThousandsSeparator = true;
             this.frequencyNumericUpDown.ValueChanged += new System.EventHandler(this.Control_TextChanged);
-            // 
+            //
             // btnOk
-            // 
+            //
             this.btnOk.DialogResult = System.Windows.Forms.DialogResult.OK;
             this.btnOk.Enabled = false;
             this.btnOk.Location = new System.Drawing.Point(149, 245);
@@ -164,9 +164,9 @@
             this.btnOk.Text = "O&K";
             this.btnOk.UseVisualStyleBackColor = true;
             this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
-            // 
+            //
             // btnCancel
-            // 
+            //
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.btnCancel.Location = new System.Drawing.Point(209, 245);
             this.btnCancel.Margin = new System.Windows.Forms.Padding(2);
@@ -175,9 +175,9 @@
             this.btnCancel.TabIndex = 6;
             this.btnCancel.Text = "&Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
-            // 
+            //
             // shiftNumericUpDown
-            // 
+            //
             this.shiftNumericUpDown.Increment = new decimal(new int[] {
             10,
             0,
@@ -200,9 +200,9 @@
             this.shiftNumericUpDown.TabIndex = 3;
             this.shiftNumericUpDown.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             this.shiftNumericUpDown.ThousandsSeparator = true;
-            // 
+            //
             // label6
-            // 
+            //
             this.label6.AutoSize = true;
             this.label6.Location = new System.Drawing.Point(10, 129);
             this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
@@ -210,9 +210,9 @@
             this.label6.Size = new System.Drawing.Size(31, 13);
             this.label6.TabIndex = 12;
             this.label6.Text = "Shift:";
-            // 
+            //
             // nudFilterBandwidth
-            // 
+            //
             this.nudFilterBandwidth.Increment = new decimal(new int[] {
             10,
             0,
@@ -230,9 +230,9 @@
             this.nudFilterBandwidth.TabIndex = 4;
             this.nudFilterBandwidth.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             this.nudFilterBandwidth.ThousandsSeparator = true;
-            // 
+            //
             // label7
-            // 
+            //
             this.label7.AutoSize = true;
             this.label7.Location = new System.Drawing.Point(10, 160);
             this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
@@ -240,9 +240,9 @@
             this.label7.Size = new System.Drawing.Size(53, 13);
             this.label7.TabIndex = 14;
             this.label7.Text = "Filter BW:";
-            // 
+            //
             // favouriteCb
-            // 
+            //
             this.favouriteCb.AutoSize = true;
             this.favouriteCb.Location = new System.Drawing.Point(85, 207);
             this.favouriteCb.Name = "favouriteCb";
@@ -250,9 +250,9 @@
             this.favouriteCb.TabIndex = 16;
             this.favouriteCb.Text = "Favourite";
             this.favouriteCb.UseVisualStyleBackColor = true;
-            // 
+            //
             // DialogEntryInfo
-            // 
+            //
             this.AcceptButton = this.btnOk;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
